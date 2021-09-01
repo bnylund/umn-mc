5106682592641110f5d3caea71c0b300790633c3
Ben Nylund <bnylund19@gmail.com>
2021-09-01 11:30:53 -0500

    Added server list event

--- src/dev/chezy/umn/UMN.java
+++ src/dev/chezy/umn/UMN.java
@@ -2,11 +2,13 @@ package dev.chezy.umn;
 
 import java.util.logging.Level;
 
+import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
+import org.bukkit.event.server.ServerListPingEvent;
 import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
@@ -35,5 +37,11 @@ public class UMN extends JavaPlugin implements Listener {
   @EventHandler
   public void onLeave(PlayerQuitEvent e) {
     e.setQuitMessage(ChatColor.GOLD + e.getPlayer().getName().toString() + ChatColor.RED.toString() + " just disconnected.");
+  }
+
+  @EventHandler
+  public void onPing(ServerListPingEvent e) {
+    e.setMaxPlayers(Bukkit.getOnlinePlayers().size() + 1);
+    e.setMotd(ChatColor.translateAlternateColorCodes('&', "   &4-= &6&lUMN Rocket League&r &4=-"));
   }
 }
